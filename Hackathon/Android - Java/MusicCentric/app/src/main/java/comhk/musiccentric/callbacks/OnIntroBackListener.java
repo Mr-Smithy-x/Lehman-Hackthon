dbb932809e69293238df06e6e28219ab3e17fd6f
Mr-Smithy-x <charltonsmith@outlook.com>
2015-10-16 14:45:47 -0400

    Updated

---
+++ Hackathon/Android - Java/MusicCentric/app/src/main/java/comhk/musiccentric/callbacks/OnIntroBackListener.java
@@ -0,0 +1,14 @@
+package comhk.musiccentric.callbacks;
+
+import android.view.View;
+
+import comhk.musiccentric.models.User;
+
+/**
+ * Created by charlton on 10/16/15.
+ */
+
+public interface OnIntroBackListener {
+    public void OnButtonClicked(View view);
+    public void OnButtonClickedFeedBack(View view, User user);
+}
